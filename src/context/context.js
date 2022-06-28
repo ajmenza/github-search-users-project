bad3a325fa37b5f8a1ec303590dbff509525e48d
ajmenza <ajm043@bucknell.edu>
2022-06-28 18:16:38 -0400

    completed project

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGithubUser = async (user) => {
+    // removes error message on next sucessful search (because of default parameters)
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((error) =>
+      console.log(error)
+    );
+
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ]).then((results) => {
+        const [response, followers] = results;
+        const status = "fulfilled";
+
+        if (repos.status === status) {
+          setRepos(repos.value.data);
+        }
+
+        if (repos.status === status) {
+          setFollowers(followers.value.data);
+        }
+      });
+    } else {
+      toggleError(true, "There is no user with that username");
+    }
+
+    checkRequests();
+    setIsLoading(false);
+  };
+
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "sorry, you have exceeded your hourly rate limit");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  useEffect(checkRequests, []);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
